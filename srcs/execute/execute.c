78f1259c069f2bcec667f0f05abea3a79725d914
Victorian Roques <victorianroques@Victorians-MacBook-Pro.local>
2021-04-08 00:48:46 +0200

    fixe redirection

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: viroques <viroques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 16:14:51 by viroques          #+#    #+#             */
-/*   Updated: 2021/04/07 22:55:40 by viroques         ###   ########.fr       */
+/*   Updated: 2021/04/08 00:45:54 by viroques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,16 +83,20 @@ static void		execute_job(t_main *m, t_node *job, int logic_type)
 	if ((logic_type == NODE_LOGIC_AMPERSTAND && m->exit_status)
 		|| (logic_type == NODE_LOGIC_PIPE && !m->exit_status))
 		return ;
-	if (job->type == NODE_PIPE)
+	if (job->type > NODE_BUILTIN)
 	{
 		std.tmp_in = dup(STDIN_FILENO);
 		std.tmp_out = dup(STDOUT_FILENO);
+	}
+	if (job->type == NODE_PIPE)
 		execute_pipe(m, job, &std);
+	else
+		execute_command(m, job);
+	if (job->type > NODE_BUILTIN)
+	{
 		dup2(std.tmp_in, STDIN_FILENO);
 		dup2(std.tmp_out, STDOUT_FILENO);
 	}
-	else
-		execute_command(m, job);
 }
 
 void			execute_command_line(t_main *m, t_node *cmd_line, int type)
