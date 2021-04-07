191b7606c7e113b962c546204892cb47450fe43d
Victorian Roques <victorianroques@Victorians-MacBook-Pro.local>
2021-04-07 23:03:07 +0200

    add execute

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: viroques <viroques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 16:14:51 by viroques          #+#    #+#             */
-/*   Updated: 2021/04/07 20:37:06 by viroques         ###   ########.fr       */
+/*   Updated: 2021/04/07 22:55:40 by viroques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,14 +83,16 @@ static void		execute_job(t_main *m, t_node *job, int logic_type)
 	if ((logic_type == NODE_LOGIC_AMPERSTAND && m->exit_status)
 		|| (logic_type == NODE_LOGIC_PIPE && !m->exit_status))
 		return ;
-	std.tmp_in = dup(STDIN_FILENO);
-	std.tmp_out = dup(STDOUT_FILENO);
 	if (job->type == NODE_PIPE)
+	{
+		std.tmp_in = dup(STDIN_FILENO);
+		std.tmp_out = dup(STDOUT_FILENO);
 		execute_pipe(m, job, &std);
+		dup2(std.tmp_in, STDIN_FILENO);
+		dup2(std.tmp_out, STDOUT_FILENO);
+	}
 	else
 		execute_command(m, job);
-	dup2(std.tmp_in, STDIN_FILENO);
-	dup2(std.tmp_out, STDOUT_FILENO);
 }
 
 void			execute_command_line(t_main *m, t_node *cmd_line, int type)
